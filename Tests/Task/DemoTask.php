25f4d5e02ad59ad72aed1ff5f3b2d7769c2c217a
Dmitry Khaperets <khaperets@gmail.com>
2016-06-07 15:06:33 +0300

    Add tests

---
+++ Tests/Task/DemoTask.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SymfonyBundles\ForkBundle\Tests\Task;
+
+use SymfonyBundles\ForkBundle\Service\TaskInterface;
+
+class DemoTask implements TaskInterface
+{
+
+    public function execute()
+    {
+        //
+    }
+
+}
